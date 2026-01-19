31931669ac7191816562379263437db75e1cd4fe
Danny Ebbutt <danny.ebbutt@airbus.com>
2026-01-19 14:22:25 +0000

    Basic express server setup

--- server/server.js
+++ server/server.js
@@ -1,1 +1,23 @@
-console.log("Hello worldddd");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001; // get from .env, or default to 3001
+
+// Middleware
+app.use(express.json());
+app.use(cors);
+
+// Error handling
+// TODO
+
+// Routes
+// TODO
+
+// Run server
+app.listen(port, () => {
+   console.log(`Server running on port ${port}`);
+});
