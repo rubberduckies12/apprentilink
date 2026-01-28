5334bfc93d9c475080c352e96bfdd14f4657afa5
Danny Ebbutt <danny.ebbutt@airbus.com>
2026-01-28 15:17:24 +0000

    Updated user model to match new schema

--- server/src/db/models/user.model.js
+++ server/src/db/models/user.model.js
@@ -1,7 +1,7 @@
 import pool from "../../db/config/db.config.js";
 
 export const getAllUsersService = async () => {
-    const result = await pool.query("SELECT * FROM users");
+    const result = await pool.query("SELECT * FROM users ORDER BY id");
     return result.rows;
 };
 
@@ -10,15 +10,16 @@ export const getUserByIdService = async (id) => {
     return result.rows[0];
 };
 
-export const createUserService = async (firstName, lastName, email) => {
-    const result = await pool.query("INSERT INTO users VALUES ($1, $2, $3) RETURNING *",
-        [firstName, lastName, email]);
+// Profile description and Postcode are optional when creating an account
+export const createUserService = async (username, email, password_hash, profile_desc = null, postcode = null) => {
+    const result = await pool.query("INSERT INTO users (username, email, password_hash, profile_description, postcode) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+        [username, email, password_hash, profile_desc, postcode]);
     return result.rows[0];
 };
 
-export const updateUserService = async (id, firstName, lastName, email) => {
-    const result = await pool.query("UPDATE users SET firstName=$1, lastName=$2, email=$3 WHERE id=$4 RETURNING *",
-        [firstName, lastName, email, id]);
+export const updateUserService = async (id, username, email, password_hash, profile_desc, postcode) => {
+    const result = await pool.query("UPDATE users SET username=$1, email=$2, password_hash=$3, profile_description=$4, postcode=$5 WHERE id=$6 RETURNING *",
+        [username, email, password_hash, profile_desc, postcode, id]);
     return result.rows[0];
 };
 
