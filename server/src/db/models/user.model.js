ac5fcdf75d2fad57fdf93f504efb8c195b456664
Danny Ebbutt <danny.ebbutt@airbus.com>
2026-01-29 16:26:19 +0000

    Added email and password validation

--- server/src/db/models/user.model.js
+++ server/src/db/models/user.model.js
@@ -19,11 +19,17 @@ export const getUserByEmailService = async (email) => {
 
 // Profile description and Postcode are optional when creating an account
 export const createUserService = async (user_type, username, email, password, profile_desc = null, postcode = null) => {
-    const password_hash = await hashPassword(password);
-
     const existingUser = await getUserByEmailService(email);
     if (existingUser)
         throw new AppError(400, "A user with this email address already exists.");
+
+    if (!email.includes('@'))
+        throw new AppError(400, "Email address is not formatted correctly.");
+
+    if (!validatePostcode(postcode))
+        throw new AppError(400, "Post Code is not formatted correctly.");
+
+    const password_hash = await hashPassword(password);
 
     const result = await pool.query("INSERT INTO users (user_type, username, email, password_hash, profile_description, postcode) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
         [user_type, username, email, password_hash, profile_desc, postcode]);
@@ -41,7 +47,13 @@ export const updateUserService = async (id, username, email, profile_desc, postc
         const existingUser = await getUserByEmailService(email);
         if (existingUser)
             throw new AppError( 400, "A user with this email address already exists.");
+
+        if (!email.includes('@'))
+            throw new AppError(400, "Email address is not formatted correctly.");
     }
+
+    if (postcode !== user.postcode && !validatePostcode(postcode))
+        throw new AppError(400, "Post Code is not formatted correctly.");
 
     const result = await pool.query("UPDATE users SET username=$1, email=$2, profile_description=$3, postcode=$4 WHERE id=$5 RETURNING *",
         [username, email, profile_desc, postcode, id]);
@@ -85,10 +97,8 @@ async function hashPassword(password) {
     return await bcrypt.hash(password, saltRounds);
 }
 
-async function validateEmail(email) {
-    // check if email address is formatted correctly
-}
-
-async function validatePostcode(postcode) {
-    // check if postcode is formatted correctly
+// Checks if a Postcode is formatted correctly
+function validatePostcode(postcode) {
+    const postcodeRegex = /^[A-Z]{1,2}\d[A-Z\d]? ?\d[A-Z]{2}$/;
+    return postcodeRegex.test(postcode);
 }
